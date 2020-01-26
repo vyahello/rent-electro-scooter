f96d1b726f3a5169b1f0b16c43160a0efe5d8c01
Volodymyr Yahello <vyahello@gmail.com>
2020-01-26 15:12:05 +0200

    Add config files sceleton

---
+++ analyse-code.sh
@@ -0,0 +1,62 @@
+#!/usr/bin/env bash
+
+PACKAGE="scooter"
+
+
+--entry-point-box() {
+:<<DOC
+    Provides pretty-printer check box
+DOC
+    printf "Start ${1} analysis ...\n"
+}
+
+
+remove-pycache() {
+:<<DOC
+    Removes python cache directories
+DOC
+    ( find . -d -name __pycache__ | xargs rm -r )
+}
+
+
+check-black() {
+:<<DOC
+    Runs "black" code analyser
+DOC
+    --entry-point-box "black" && ( black --check ${PACKAGE} )
+}
+
+
+check-flake() {
+:<<DOC
+    Runs "flake8" code analyser
+DOC
+    --entry-point-box "flake" && ( flake8 ${PACKAGE} )
+}
+
+check-pylint() {
+:<<DOC
+    Runs "pylint" code analyser
+DOC
+    --entry-point-box "pylint" && ( pylint $(find ./ -iname *.py) )
+}
+
+
+check-unittests() {
+:<<DOC
+    Runs unittests using "pytest" framework
+DOC
+    --entry-point-box "unitests" && pytest
+}
+
+
+main() {
+:<<DOC
+    Runs "main" code analyser
+DOC
+    remove-pycache
+    check-black && check-flake && check-pylint && check-unittests
+}
+
+
+main
